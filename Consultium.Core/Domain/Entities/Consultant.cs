9ecf139d65df43b6ee051e520532898408f43188
Jakob Bergström <jakob.bergstrom@live.se>
2023-01-23 11:37:11 +0100

    started adding data annotations to entities

--- Consultium.Core/Domain/Entities/Consultant.cs
+++ Consultium.Core/Domain/Entities/Consultant.cs
@@ -1,12 +1,25 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
-    public class Consultant
-    {
-        
-    }
+  [Table("Consultant")]
+  public class Consultant
+  {
+    [Key]
+    public Guid ConsultantId { get; set; }
+    [Required]
+    [StringLength(50)]
+    public string FirstName { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    public string LastName { get; set; }
+    [Required]
+    public Boolean HasAsignment { get; set; }
+  }
 }
--- Consultium.Core/Domain/Entities/Customer.cs
+++ Consultium.Core/Domain/Entities/Customer.cs
@@ -1,12 +1,20 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
+  [Table("Customer")]
   public class Customer
   {
+    [Key]
+    public Guid CustomerId { get; set; }
 
+    [Required]
+    [MaxLength(50)]
+    public string CustomerName { get; set; }
   }
 }
